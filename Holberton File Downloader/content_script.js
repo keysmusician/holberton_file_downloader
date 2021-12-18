df90d5707c256904655aaec7a0b4f8fa76ef1412
Justin Masayda <justin.masayda@gmail.com>
2021-12-18 04:04:35 -0500

    Add find files button & display filenames in popup

--- Holberton File Downloader/content_script.js
+++ Holberton File Downloader/content_script.js
@@ -1,42 +1,22 @@
-function download (data, filename, type) {
-  const file = new Blob([data], { type: type });
-  const a = document.createElement('a');
-  const url = URL.createObjectURL(file);
-  a.href = url;
-  a.download = filename;
-  document.body.appendChild(a);
-  a.click();
-  setTimeout(function () {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 0);
-}
 chrome.runtime.sendMessage({ message: 'activate_icon' });
-chrome.runtime.onMessage.addListener((msg, sender) => {
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if ((msg.from === 'popup') && (msg.subject === 'requestFilenames')) {
-    const file_li = document.querySelectorAll(
+    const filenameListItems = document.querySelectorAll(
       'div.task-card div.list-group div.list-group-item ul li:last-child');
 
-    const unique_filenames = new Set();
+    const uniqueFilenames = new Set();
 
-    file_li.forEach(li => {
+    filenameListItems.forEach(li => {
       if (li.firstChild.textContent === 'File: ') {
-        const li_text = li.lastChild.textContent;
-        unique_filenames.add(li_text);
+        const filename = li.lastChild.textContent;
+        // Sometimes multiple files are named in the same list item.
+        // Those should be split into separate strings:
+        filename.split(', ').forEach(split => {
+          uniqueFilenames.add(split);
+        });
       }
     });
 
-    // Sometimes, multiple files are named in the same list item.
-    // Those should be split into separate strings:
-    const string_splits = new Set();
-    unique_filenames.forEach(filename => {
-      filename.split(', ').forEach(split => {
-        string_splits.add(split);
-      });
-    });
-
-    string_splits.forEach(filename => {
-      download('', filename, 'text');
-    });
+    sendResponse({ files: Array.from(uniqueFilenames) });
   }
 });
--- Holberton File Downloader/popup.js
+++ Holberton File Downloader/popup.js
@@ -1,11 +1,53 @@
-document.getElementById('downloadButton').addEventListener('click', () => {
+let files;
+const findButton = document.getElementById('findButton');
+findButton.addEventListener('click', () => {
   chrome.tabs.query({
     active: true,
     currentWindow: true
   }, tabs => {
     chrome.tabs.sendMessage(
       tabs[0].id,
-      { from: 'popup', subject: 'requestFilenames' }
+      { from: 'popup', subject: 'requestFilenames' },
+      (response) => {
+        const ul = document.getElementById('files');
+
+        // Reset the ul before adding new elements:
+        ul.textContent = '';
+
+        files = response.files;
+        if (response.files.length === 0) {
+          downloadButton.style.display = 'none';
+          ul.appendChild(document.createTextNode('No files found'));
+        } else {
+          downloadButton.style.display = 'block';
+          response.files.forEach(filename => {
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(filename));
+            ul.appendChild(li);
+          });
+        }
+      }
     );
+  });
+});
+
+function download (data, filename, type) {
+  const file = new Blob([data], { type: type });
+  const a = document.createElement('a');
+  const url = URL.createObjectURL(file);
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(function () {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 0);
+}
+
+const downloadButton = document.getElementById('downloadButton');
+downloadButton.addEventListener('click', () => {
+  files.forEach(filename => {
+    download('', filename, 'text');
   });
 });
